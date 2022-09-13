a8022e6fd3e096ae6009954614bb6e4512b34b12
Andrés Pujol <andrespujol@gmail.com>
2022-09-13 00:14:01 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HomeView msg="Delicias"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import HomeView from './components/HomeView.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    HomeView,
   },
 };
 </script>
---
+++ src/components/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>{{ title }}</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  props: {
+    title: String,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
